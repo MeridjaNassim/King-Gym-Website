67f8540b656225a245d403ec6a526cecef15ef43
MeridjaNassim <ha_meridja@esi.dz>
2020-10-03 19:46:44 +0100

    Initial Commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,33 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
+import { PropsWithChildren } from 'react'
 import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-type Props = {
-  children?: ReactNode
-  title?: string
+interface Props {
+  title?: string,
+  transparent?: boolean
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+const Layout = (props: PropsWithChildren<Props>) => {
+
+
+  return (
+
+    <div>
+      <Head>
+        <title>{props.title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <Navbar transparent={true} />
+      </header>
+      <main>
+        {props.children}
+      </main>
+      <Footer></Footer>
+    </div>
+  )
+}
 
 export default Layout
